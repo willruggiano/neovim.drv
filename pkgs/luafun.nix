9ddef828183d4c27c8764cb2b27a5eef5cfcc8eb
Will Ruggiano <wmruggiano@gmail.com>
2023-04-11 12:07:56 -0600

    feat: use niv for luafun

--- pkgs/luafun.nix
+++ pkgs/luafun.nix
@@ -3,24 +3,15 @@
   fetchFromGitHub,
   fetchurl,
   lua,
-}:
-buildLuarocksPackage rec {
-  pname = "fun";
-  version = "scm-1";
+}: let
+  sources = import ../nix/sources.nix;
+in
+  buildLuarocksPackage {
+    pname = "fun";
+    version = "scm-1";
 
-  knownRockspec =
-    (fetchurl {
-      url = "mirror://luarocks/fun-${version}.rockspec";
-      hash = "sha256-35RcuCYBf5nSO65qBYLYNWmRCaCbFqQdXrk3wBy/lQU=";
-    })
-    .out;
+    src = sources.luafun;
+    knownRockspec = "${sources.luafun}/fun-scm-1.rockspec";
 
-  src = fetchFromGitHub {
-    owner = "luafun";
-    repo = "luafun";
-    rev = "cb6a7e25d4b55d9578fd371d1474b00e47bd29f3";
-    hash = "sha256-lqWTPn1HPQxhfkUFvEUCbS05IkkroaykgYehJqQ0+lw=";
-  };
-
-  propagatedBuildInputs = [lua];
-}
+    propagatedBuildInputs = [lua];
+  }

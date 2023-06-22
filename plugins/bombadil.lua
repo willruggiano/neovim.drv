285bd02a4bb310aaf1b130656a535fc703798afc
Will Ruggiano <wmruggiano@gmail.com>
2023-06-22 01:12:39 -0600

    plug: overseer vscode defaults

--- plugins/bombadil.lua
+++ plugins/bombadil.lua
@@ -1,3 +1,27 @@
 return function()
   vim.cmd.colorscheme "flavours"
+
+  local vim_to_sys = {
+    [vim.log.levels.TRACE] = "low",
+    [vim.log.levels.DEBUG] = "low",
+    [vim.log.levels.INFO] = "normal",
+    [vim.log.levels.WARN] = "normal",
+    [vim.log.levels.ERROR] = "critical",
+  }
+
+  local notify = vim.notify
+  local function notify_send(msg, level, opts)
+    if not pcall(os.execute, string.format("notify-send -u %s Neovim '%s'", vim_to_sys[level], msg)) then
+      notify(msg, level, opts)
+    end
+  end
+
+  local notifiers = {
+    [vim.log.levels.WARN] = notify_send,
+    [vim.log.levels.ERROR] = notify_send,
+  }
+  ---@diagnostic disable-next-line: duplicate-set-field
+  vim.notify = function(msg, level, opts)
+    (notifiers[level] or notify)(msg, level, opts)
+  end
 end
--- plugins/overseer.lua
+++ plugins/overseer.lua
@@ -1,13 +1,26 @@
 return function()
-  require("overseer").setup()
+  require("overseer").setup {
+    component_aliases = {
+      default = {
+        { "display_duration", detail_level = 2 },
+        "on_output_summarize",
+        "on_exit_set_status",
+        "on_complete_notify",
+      },
+      default_vscode = {
+        "default",
+        { "on_result_diagnostics_quickfix", open = true },
+      },
+    },
+  }
 
   vim.api.nvim_create_user_command("Run", function(opts)
     local args = vim.fn.expandcmd(opts.args)
     local task = require("overseer").new_task {
       cmd = args,
       components = {
-        { "on_output_quickfix", open = opts.bang },
         "default",
+        { "on_output_quickfix", open = opts.bang },
       },
     }
     task:start()
@@ -16,6 +29,10 @@ return function()
   local nnoremaps = require("bombadil.lib.keymap").nnoremaps
 
   nnoremaps {
+    ["<space>r"] = {
+      "<cmd>OverseerRun<cr>",
+      { desc = "Run" },
+    },
     ["<space>t"] = {
       function()
         require("overseer.commands")._toggle { bang = true, args = "bottom" }

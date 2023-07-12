48857b71e4b0485c645e55ef215f6748ac623f43
Will Ruggiano <wmruggiano@gmail.com>
2023-07-11 22:22:21 -0600

    fix: normal toggleterm

--- plugins/toggleterm.lua
+++ plugins/toggleterm.lua
@@ -8,6 +8,6 @@ return function()
   local nnoremap = require("bombadil.lib.keymap").nnoremap
 
   nnoremap("<space><space>", function()
-    toggleterm.toggle_command("size=20", vim.api.nvim_get_current_tabpage())
+    toggleterm.toggle_command("size=20", vim.v.count)
   end, { desc = "Toggle terminal" })
 end

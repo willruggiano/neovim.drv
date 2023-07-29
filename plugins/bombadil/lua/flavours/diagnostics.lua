c5ef330e48b6cb94827b1ef528817cbfeb6f6f44
Will Ruggiano <wmruggiano@gmail.com>
2023-07-29 01:47:08 -0600

    fix: diagnostics

--- plugins/bombadil/lua/flavours/diagnostics.lua
+++ plugins/bombadil/lua/flavours/diagnostics.lua
@@ -3,13 +3,20 @@ local hi = require("flavours").highlight
 local M = {}
 
 function M.setup(palette)
+  local d = {
+    ok = palette.base0B,
+    error = palette.base08,
+    warn = palette.base0E,
+    info = palette.base05,
+    hint = palette.base0C,
+  }
+
   -- See: |diagnostic-highlights|
-  hi.DiagnosticOk = { fg = palette.base0B }
-  hi.DiagnosticError = { fg = palette.base08 }
-  hi.DiagnosticWarn = { fg = palette.base0E }
-  hi.DiagnosticInfo = { fg = palette.base05 }
-  hi.DiagnosticHint = { fg = palette.base0C }
-  hi.DiagnosticUnnecessary = "DiagnosticWarn"
+  hi.DiagnosticOk = { fg = d.ok }
+  hi.DiagnosticError = { fg = d.error }
+  hi.DiagnosticWarn = { fg = d.warn }
+  hi.DiagnosticInfo = { fg = d.info }
+  hi.DiagnosticHint = { fg = d.hint }
 
   -- hi.DiagnosticVirtualTextOk = "DiagnosticOk"
   -- hi.DiagnosticVirtualTextError = "DiagnosticError"
@@ -17,11 +24,12 @@ function M.setup(palette)
   -- hi.DiagnosticVirtualTextInfo = "DiagnosticInfo"
   -- hi.DiagnosticVirtualTextHint = "DiagnosticHint"
 
-  hi.DiagnosticUnderlineOk = { undercurl = true }
-  hi.DiagnosticUnderlineError = { undercurl = true }
-  hi.DiagnosticUnderlineWarn = { undercurl = true }
-  hi.DiagnosticUnderlineInfo = { undercurl = true }
-  hi.DiagnosticUnderlineHint = { undercurl = true }
+  hi.DiagnosticUnderlineOk = { sp = d.ok, undercurl = true }
+  hi.DiagnosticUnderlineError = { sp = d.error, undercurl = true }
+  hi.DiagnosticUnderlineWarn = { sp = d.warn, undercurl = true }
+  hi.DiagnosticUnderlineInfo = { sp = d.info, undercurl = true }
+  hi.DiagnosticUnderlineHint = { sp = d.hint, undercurl = true }
+  hi.DiagnosticUnnecessary = { sp = d.hint, undercurl = true }
 end
 
 return M

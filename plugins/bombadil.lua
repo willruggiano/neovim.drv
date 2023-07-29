e09b6c22e6322ecc68ccf444d1bb6450a91e6d5d
Will Ruggiano <wmruggiano@gmail.com>
2023-07-29 14:04:08 -0600

    feat: override background

--- plugins/bombadil.lua
+++ plugins/bombadil.lua
@@ -1,5 +1,8 @@
 return function()
   vim.opt.background = (function()
+    if vim.env.BG ~= nil then
+      return vim.env.BG
+    end
     local t = os.date "*t"
     -- Use light theme between 8am and 8pm, light theme otherwise
     if t.hour < 8 or t.hour > 20 then

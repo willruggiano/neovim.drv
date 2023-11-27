9f2e23f47bcf135449422f4114112a3fd2380a8a
Will Ruggiano <wmruggiano@gmail.com>
2023-11-27 10:42:28 -0700

    fix(dbee): nonumber, norelativenumber, signcolumn=no

--- plugins/dbee.lua
+++ plugins/dbee.lua
@@ -23,6 +23,20 @@ return function()
     sources = {
       require("dbee.sources").FileSource:new(vim.fn.getcwd() .. "/.db.json"),
     },
+    ui = {
+      -- I can manage my own window layouts thank you very much
+      pre_open_hook = function() end,
+      post_close_hook = function() end,
+      window_commands = {
+        drawer = function()
+          vim.cmd "topleft 40vsplit"
+          vim.api.nvim_set_option_value("relativenumber", false, {})
+          vim.api.nvim_set_option_value("number", false, {})
+          vim.api.nvim_set_option_value("signcolumn", "no", {})
+          return vim.api.nvim_get_current_win()
+        end,
+      },
+    },
   }
 
   local nnoremap = require("bombadil.lib.keymap").nnoremap

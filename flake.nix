8f1902b02c60b6f3381929a16e4d8e461a02027b
Will Ruggiano <wmruggiano@gmail.com>
2024-05-20 14:16:22 -0600

    feat: hercules-ci

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     devenv.url = "github:cachix/devenv";
     git-hooks.url = "github:cachix/git-hooks.nix";
+    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
     neovim-nix = {
       url = "github:willruggiano/neovim.nix";
       inputs.flake-parts.follows = "flake-parts";
@@ -36,6 +37,7 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.devenv.flakeModule
+        inputs.hercules-ci-effects.flakeModule
         inputs.neovim-nix.flakeModule
         ./modules
       ];
@@ -119,6 +121,17 @@
           nvim = config.neovim.final;
           nvim-dbee = pkgs.callPackage ./pkgs/nvim-dbee.nix {};
           nvim-treesitter = pkgs.callPackage ./pkgs/nvim-treesitter {};
+        };
+      };
+
+      hercules-ci.flake-update = {
+        enable = true;
+        baseMerge.enable = true;
+        baseMerge.method = "rebase";
+        autoMergeMethod = "rebase";
+        when = {
+          dayOfWeek = ["Mon"];
+          hour = [0];
         };
       };
     };

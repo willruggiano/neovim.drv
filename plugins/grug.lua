79abc930f1f09f4222752d5236faa6baecefd04b
Will Ruggiano <wmruggiano@gmail.com>
2025-01-13 23:16:33 -0800

    fix: grug

--- plugins/grug.lua
+++ plugins/grug.lua
@@ -1,17 +1,4 @@
 return function()
-  vim.api.nvim_create_user_command("Float", function()
-    local bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_open_win(bufnr, true, {
-      border = "single",
-      style = "minimal",
-      relative = "editor",
-      height = vim.o.lines,
-      width = math.floor(vim.o.columns * 0.3),
-      row = 0,
-      col = vim.o.columns - 1,
-    })
-  end, {})
-
   local grug = require "grug-far"
   grug.setup {
     debounceMs = 250,
@@ -26,26 +13,6 @@ return function()
       openPrevLocation = { n = "<C-p>" },
       previewLocation = false,
     },
-    startInInsertMode = false,
     transient = true,
-    windowCreationCommand = "Float",
   }
-
-  local noremap = require("bombadil.lib.keymap").noremap
-  local instanceName = "global"
-
-  -- noremap({ "n", "v" }, "<space>f", function()
-  --   if vim.fn.mode():lower():find "v" ~= nil then
-  --     -- Kill the existing instance, if any, in visual mode as we expect the
-  --     -- visual selection to have changed
-  --     grug.kill_instance(instanceName)
-  --   end
-  --   if grug.has_instance(instanceName) then
-  --     -- If the global instance exists, open it
-  --     grug.open_instance(instanceName)
-  --   else
-  --     -- Otherwise go through the normal entrypoint
-  --     grug.open { instanceName = instanceName }
-  --   end
-  -- end, { desc = "Find" })
 end

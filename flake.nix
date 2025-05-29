55736f16dc738af5c4f4884e7aa9bcd15e95c59f
Will Ruggiano <mix.spend.dough@usecloaked.com>
2025-05-29 09:27:40 -0700

    chore: update some workflows

--- flake.nix
+++ flake.nix
@@ -94,42 +94,12 @@
             };
         };
 
-        apps = {
-          push.program = pkgs.writeShellApplication {
-            name = "push.sh";
-            runtimeInputs = with pkgs; [cachix jq];
-            text = ''
-              nix flake archive --json \
-              | jq -r '.path,(.inputs|to_entries[].value.path)' \
-              | cachix push willruggiano;
-
-              nix build --json \
-              | jq -r '.[].outputs | to_entries[].value' \
-              | cachix push willruggiano;
-
-              cachix pin willruggiano nvim-drv "$(nix build --print-out-paths)"
-            '';
-          };
-          update.program = pkgs.writeShellApplication {
-            name = "update.sh";
-            runtimeInputs = with pkgs; [niv nix-update];
-            text = ''
-              nix flake update &&
-              niv update &&
-              nix run .#nvim-treesitter.updateScript &&
-              nix-update --flake nvim-dbee --version=branch --subpackage dbee &&
-              nix-update --flake sqruff --version-regex='v(.*)' --override-filename pkgs/sqruff.nix &&
-              nix flake check
-            '';
-          };
-        };
-
         devShells.default = pkgs.mkShell {
           name = "neovim";
           inputsFrom = [
             config.pre-commit.devShell
           ];
-          buildInputs = with pkgs; [alejandra just niv nix-update];
+          buildInputs = with pkgs; [alejandra cachix jq just niv nix-update];
         };
 
         packages = {
--- justfile
+++ justfile
@@ -5,5 +5,19 @@ install:
 build:
   nom build
 
+push:
+  nix flake check
+  nix flake archive --json | jq -r '.path,(.inputs|to_entries[].value.path)' | cachix push willruggiano
+  nix build --json | jq -r '.[].outputs | to_entries[].value' | cachix push willruggiano
+  cachix pin willruggiano nvim-drv "$(nix build --print-out-paths)"
+
 run:
   nix run
+
+update:
+  nix flake update
+  niv update
+  nix run .#nvim-treesitter.updateScript
+  nix-update --flake nvim-dbee --version=branch --subpackage dbee
+  nix-update --flake sqruff --version-regex='v(.*)' --override-filename pkgs/sqruff.nix
+  nix flake check

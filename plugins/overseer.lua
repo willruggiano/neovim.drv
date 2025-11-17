f4d52293e801f337d03d2360f0e6b68344eb1d34
Will Ruggiano <mix.spend.dough@usecloaked.com>
2025-11-16 23:21:28 -0800

    chore(overseer): v2

--- plugins/overseer.lua
+++ plugins/overseer.lua
@@ -1,16 +1,11 @@
 return function()
-  local overseer = require "overseer"
-
-  overseer.setup()
-
-  vim.keymap.set("n", "<space><cr>", function()
-    local tasks = overseer.list_tasks { recent_first = true }
-    if not vim.tbl_isempty(tasks) then
-      overseer.run_action(tasks[1], "restart")
-    end
-  end, { desc = "[overseer] run last" })
-
-  vim.keymap.set("n", "<space>r", "<cmd>OverseerRun<cr>", { desc = "[overseer] run" })
-
-  vim.keymap.set("n", "<space>t", "<cmd>OverseerToggle bottom<cr>", { desc = "[overseer] toggle" })
+  require("overseer").setup()
+  vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<cr>", { desc = "OverseerRun" })
+  vim.keymap.set("n", "<leader>os", function()
+    vim.ui.input({ prompt = "> ", completion = "shellcmdline" }, function(cmd)
+      local task = require("overseer").new_task { cmd = cmd }
+      task:start()
+    end)
+  end, { desc = "OverseerShell" })
+  vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle!<cr>", { desc = "OverseerToggle!" })
 end

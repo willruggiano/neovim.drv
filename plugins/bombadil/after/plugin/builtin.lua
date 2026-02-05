944c5f66cdf52f3c2f0fbab761a38dcdeed4cb5e
Will Ruggiano <mix.spend.dough@usecloaked.com>
2026-02-05 07:57:07 -0800

    feat: -

--- plugins/bombadil/after/plugin/builtin.lua
+++ plugins/bombadil/after/plugin/builtin.lua
@@ -3,3 +3,4 @@ vim.cmd.packadd "cfilter"
 vim.g.netrw_liststyle = 3 -- tree
 vim.g.netrw_list_hide = vim.fn["netrw_gitignore#Hide"]()
 vim.cmd.packadd "netrw"
+vim.keymap.set("n", "-", ":Explore<cr>", { desc = ":Explore" })
